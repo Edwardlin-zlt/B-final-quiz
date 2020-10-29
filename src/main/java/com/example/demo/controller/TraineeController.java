731b4c6f4787efd7ae4f1b7d17240818b23b76e0
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 11:26:44 +0800

    [Lintao] feat: Init Trainee Controller Service and Repository

---
+++ src/main/java/com/example/demo/controller/TraineeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.TraineeService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TraineeController {
+
+    private final TraineeService traineeService;
+
+    public TraineeController(TraineeService traineeService) {
+        this.traineeService = traineeService;
+    }
+}
