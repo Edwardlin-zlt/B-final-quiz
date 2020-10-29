8a42510e2a30f445912fe71e1b80afd10cc6b435
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 11:34:35 +0800

    [Lintao] feat: add runtime config in gradle & RestApi tested

--- src/main/java/com/example/demo/controller/TraineeController.java
+++ src/main/java/com/example/demo/controller/TraineeController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.TraineeService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TraineeController {
 
     private final TraineeService traineeService;
@@ -11,4 +15,10 @@ public class TraineeController {
     public TraineeController(TraineeService traineeService) {
         this.traineeService = traineeService;
     }
+
+    @GetMapping("/trainees")
+    public String test() {
+        return "Hello";
+    }
+
 }
