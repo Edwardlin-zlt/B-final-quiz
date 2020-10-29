f61d8bcb1e1feb47106f39d3ff140961e6f5b1d8
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 11:43:04 +0800

    [Lintao] feat: Relationship between trainee and group entity

--- src/main/java/com/example/demo/entity/Group.java
+++ src/main/java/com/example/demo/entity/Group.java
@@ -1,4 +1,21 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Group {
+    private Long id;
+    @OneToMany
+    private Trainee trainee;
 }
--- src/main/java/com/example/demo/entity/Trainee.java
+++ src/main/java/com/example/demo/entity/Trainee.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -20,4 +17,6 @@ public class Trainee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     private String name;
+    @ManyToOne
+    private Group group;
 }
