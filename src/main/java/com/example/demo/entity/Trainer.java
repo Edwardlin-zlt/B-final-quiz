875a54a13da607a1fc4da497eb75e43d70627050
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 14:58:05 +0800

    [Lintao] feat: add trainer entity

--- src/main/java/com/example/demo/entity/Trainer.java
+++ src/main/java/com/example/demo/entity/Trainer.java
@@ -1,4 +1,22 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Trainer {
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    @ManyToOne
+    private Group group;
 }
--- src/test/java/com/example/demo/controller/TraineeControllerTest.java
+++ src/test/java/com/example/demo/controller/TraineeControllerTest.java
@@ -1,13 +1,8 @@
 package com.example.demo.controller;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.Mockito.*;
-
-import com.example.demo.entity.Group;
 import com.example.demo.entity.Trainee;
 import com.example.demo.exception.TraineeNotExistException;
 import com.example.demo.service.TraineeService;
-import org.graalvm.compiler.nodes.java.ArrayLengthNode;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -26,7 +21,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
