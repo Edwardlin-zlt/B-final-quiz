f6a4399f82e4c1beaebdd28783a3e009a8e35c90
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 11:22:38 +0800

    [Lintao] feat: add fields of Trainee

--- src/main/java/com/example/demo/entity/Trainee.java
+++ src/main/java/com/example/demo/entity/Trainee.java
@@ -1,5 +1,23 @@
 package com.example.demo.entity;
 
-public class Trainee {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Trainee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
 }
