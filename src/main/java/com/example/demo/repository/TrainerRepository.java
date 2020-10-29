74f34a6ac3048cd340fd3920b0e98ef742859476
lintao <lintao.zhang@thoughtworks.com>
2020-10-29 15:01:14 +0800

    [Lintao] feat: add Trianer repository

---
+++ src/main/java/com/example/demo/repository/TrainerRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Trainee;
+import com.example.demo.entity.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+    @Query("select t from Trainer t where t.group is null")
+    List<Trainee> findUngroupedTrainers();
+
+    @Query("select t from Trainer t where t.group is not null")
+    List<Trainee> findGroupedTrainers();
+}
